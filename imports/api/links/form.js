8a104ea851c939f3835d210dda6dc961a465cf4c
Cris <bigc4997@gmail.com>
2019-10-16 16:26:56 -0400

    refactoring js and pages

---
+++ imports/api/links/form.js
@@ -0,0 +1,5 @@
+// Definition of the form collection
+
+import { Mongo } from 'meteor/mongo';
+
+export const Form = new Mongo.Collection('forms');
--- imports/ui/components/submitquote/submitquote.js
+++ imports/ui/components/submitquote/submitquote.js
@@ -2,30 +2,88 @@ import { Template } from 'meteor/templating';
 
 import './submitquote.html'
 
-
+var currentTab = 0;
 
 Template.SubmitQuote.events({
-  'click button#nextBtn'(e){
+  'click button#nextBtn'(e, template){
     e.preventDefault();
-    this.phase1();
+    var x = template.findAll('.h');
+    var n = currentTab;
+
+    console.log(n);
+    console.log(x.length);
+    if (currentTab >= (x.length -1)) {
+      console.log('submit');
+      // ... the form gets submitted:
+      // template.find(".home").submit();
+      return false;
+    }
+
+    x[n].style.display = "none";
+
+    currentTab++;
+    n = currentTab;
+
+
+    x[n].style.display = "block";
+    if (n == 0) {
+    template.find("#prevBtn").style.display = "none";
+    } else {
+    template.find("#prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    template.find("#nextBtn").innerHTML = "Submit";
+  } else {
+    template.find("#nextBtn").innerHTML = "Next";
+  }
+
+
 },
 
+'click button#prevBtn'(e, template){
+  e.preventDefault();
+  var x = template.findAll('.h');
+  var n = currentTab;
+  x[n].style.display = "none";
+
+  currentTab--;
+  n = currentTab;
+
+
+  x[n].style.display = "block";
+  if (n == 0) {
+  template.find("#prevBtn").style.display = "none";
+  } else {
+  template.find("#prevBtn").style.display = "inline";
+}
+if (n == (x.length - 1)) {
+  template.find("#nextBtn").innerHTML = "Submit";
+} else {
+  template.find("#nextBtn").innerHTML = "Next";
+}
+},
 
 })
 
-Template.SubmitQuote.helpers({
-  // var template = this;
-  // var flavor,
-  //     filling,
-  //     firstName,
-  //     lastName,
-  //     email,
-  //     img,
-  //     description;
-
-  phase1(){
-    var x = this.findAll('#phase');
-    console.log(x);
+Template.SubmitQuote.onRendered(function () {
+  var template = this;
+  var n = currentTab;
+    var x = template.findAll('.h');
+    console.log(x.length);
+    x[n].style.display = "block";
+    if (n == 0) {
+    template.find("#prevBtn").style.display = "none";
+    } else {
+    template.find("#prevBtn").style.display = "inline";
   }
+  if (n == (x.length - 1)) {
+    template.find("#nextBtn").innerHTML = "Submit";
+  } else {
+    template.find("#nextBtn").innerHTML = "Next";
+  }
+});
+
+Template.SubmitQuote.helpers({
+
 
 })
